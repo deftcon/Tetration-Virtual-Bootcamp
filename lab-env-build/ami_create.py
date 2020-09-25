5edd7b0eb2dddf2fc95beb4188b3fe52084f2c6d
mcmadden <mike@deftconsult.io>
2020-09-24 20:58:08 -0400

    select correct EKS AMI based on region and populate parameters.yml

--- lab-env-build/ami_create.py
+++ lab-env-build/ami_create.py
@@ -116,6 +116,16 @@ for ami_key in ami_dict.keys():
         if x == len(params_file) - 1 and not replaced:
             params_file.append(f"{ami_key}: {ami_dict[ami_key]['new_id']}\n")
 
+# Parse list of valid EKS worker nodes and update parameters.yml
+with open ('eks_worker_amis.yml', 'r') as f:
+    eks_amis = yaml.safe_load(f)
+
+params_file.append(f"{aws_region}: {eks_amis.get(aws_region)}")
+logger.info(f"EKS image for region {aws_region} is {eks_amis.get(aws_region)}")
+
+
+
+
 with open('parameters.yml', 'w') as f:
     f.writelines(params_file)
 
